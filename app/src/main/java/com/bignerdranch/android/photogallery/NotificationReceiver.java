0dc6fb730335e13239911b9cdffe5fe6d15a1032
IFang Lee <ilee@ifwe.co>
2017-06-29 18:00:03 -0700

    add broadcast intent

---
+++ app/src/main/java/com/bignerdranch/android/photogallery/NotificationReceiver.java
@@ -0,0 +1,34 @@
+package com.bignerdranch.android.photogallery;
+
+import android.app.Activity;
+import android.app.Notification;
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.support.v4.app.NotificationManagerCompat;
+import android.util.Log;
+
+/**
+ * . This broadcast intent will often be sent right before PollService dies.
+ * That means that your broadcast receiver might be dead, too.
+ Thus, your final broadcast receiver will need to be a standalone receiver,
+ and you will need to enforce that it runs after the dynamically
+ registered receiver by different means.
+ */
+public class NotificationReceiver extends BroadcastReceiver {
+    private static final String TAG = "NotificationReceiver";
+    @Override
+    public void onReceive(Context context, Intent intent) {
+        Log.i(TAG, "received result: " + getResultCode());
+        if (getResultCode() != Activity.RESULT_OK) {
+            // A foreground activity cancelled the broadcast
+            return;
+        }
+
+        int requestCode = intent.getIntExtra(PollService.REQUEST_CODE, 0);
+        Notification notification = intent.getParcelableExtra(PollService.NOTIFICATION);
+
+        NotificationManagerCompat notificationManager = NotificationManagerCompat.from(context);
+        notificationManager.notify(requestCode, notification);
+    }
+}
